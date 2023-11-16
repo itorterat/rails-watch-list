c6cf021d44041d34b9dfd17139a534bcd7be8750
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-11-16 15:19:41 +0100

    Add seeding for movies
    Signed-off-by: Irvin TORTERAT <irvin.torterat@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'net/http'
+
+url = URI('http://tmdb.lewagon.com/movie/top_rated')
+
+top_movies = JSON.parse(URI.open(url).read)['results']
+
+puts 'Cleaning database...'
+Movie.destroy_all
+
+puts "Creating #{top_movies.size} movies..."
+top_movies.each do |movie|
+  Movie.create(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts 'Finished!'
